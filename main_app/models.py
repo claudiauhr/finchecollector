5565e02c234787a34fe1ba69d0d523ffed6b8f52
claudiauhr <claudia.c.uhr@gmail.com>
2023-02-06 00:36:11 -0500

    add files

---
+++ main_app/models.py
@@ -0,0 +1,46 @@
+from django.db import models
+from django.urls import reverse
+from datetime import date
+
+# Create your models here.
+
+
+class Toy(models.Model):
+    name = models.CharField(max_length=50)
+    color = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f'{self.color} {self.name}'
+
+    def get_absolute_url(self):
+        return reverse('toys_detail', kwargs={'pk': self.id})
+
+
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    species = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={"finch_id": self.id})
+
+
+class Feeding(models.Model):
+    MEALS = (
+        ('B', 'Breakfast'),
+        ('L', 'Lunch'),
+        ('D', 'Dinner')
+    )
+    date = models.DateField('feeding date')
+    meal = models.CharField(max_length=1, choices=MEALS, default=MEALS[0][0])
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ('-date',)
