f6a5bc6b0e276b3e2385f57a994ea140bd78265f
claudiauhr <claudia.c.uhr@gmail.com>
2023-02-06 01:22:02 -0500

    add auth

--- main_app/views.py
+++ main_app/views.py
@@ -3,6 +3,10 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
 from .models import Finch, Toy
 from .forms import FeedingForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 def home(request):
@@ -36,40 +40,60 @@ def add_feeding(request, finch_id):
     return redirect('detail', finch_id=finch_id)
 
 
-class FinchCreate(CreateView):
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+
+class FinchCreate(LoginRequiredMixin, CreateView):
     model = Finch
     fields = '__all__'
     success_url = '/finches/'
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
-class FinchUpdate(UpdateView):
+
+class FinchUpdate(LoginRequiredMixin, UpdateView):
     model = Finch
     fields = ['species', 'description', 'age']
 
 
-class FinchDelete(DeleteView):
+class FinchDelete(LoginRequiredMixin, DeleteView):
     model = Finch
     success_url = '/finches/'
 
 
-class ToysIndex(ListView):
+class ToysIndex(LoginRequiredMixin, ListView):
     model = Toy
 
 
-class ToysDetail(DetailView):
+class ToysDetail(LoginRequiredMixin, DetailView):
     model = Toy
 
 
-class ToyCreate(CreateView):
-    model = Toy
-    fields = '__all__'
-
-
-class ToyUpdate(UpdateView):
+class ToyCreate(LoginRequiredMixin, CreateView):
     model = Toy
     fields = '__all__'
 
 
-class ToyDelete(DeleteView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyDelete(LoginRequiredMixin, DeleteView):
     model = Toy
     success_url = '/toys/'
